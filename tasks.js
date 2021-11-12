4b6648bc82602d65d92baa1ded33a45997ff87f5
Lamont Tanisha <ltanisha@ev-h2.kzn.21-school.ru>
2021-11-12 19:24:42 +0300

    t1

--- tasks.js
+++ tasks.js
@@ -1,4 +1,5 @@
-// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// Consider an array/list of sheep where some sheep may be missing from their place. 
+// We need a function that counts the number of sheep present in the array (true means present).
 
 function countSheeps(arrayOfSheep) {
 	let j = 0;
@@ -10,7 +11,8 @@ function countSheeps(arrayOfSheep) {
 	return (j);
   }
 
-// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// In this kata you will create a function that takes a list of non-negative integers and 
+// strings and returns a new list with the strings filtered out.
 
   function filter_list(l) {
 	// Return a new array with the strings filtered out
@@ -18,4 +20,3 @@ function countSheeps(arrayOfSheep) {
 	return res;
   }
 
-  

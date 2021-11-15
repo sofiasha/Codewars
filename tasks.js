2a49fd43a7e28bec6dacc62e136319e7a283135a
Lamont Tanisha <ltanisha@ev-b2.kzn.21-school.ru>
2021-11-15 20:57:00 +0300

    task2

--- tasks.js
+++ tasks.js
@@ -1,3 +1,16 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. 
+// This number is the position the word should have in the result.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+//If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// e.g. "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+
+function order(words){
+	return words.split(' ').sort(function(a,b)
+	{
+	  return a.match(/\d/) - b.match(/\d/);
+	}).join(' ');
+	}
+
 // ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 // If the function is passed a valid PIN string, return true, else return false.
 
